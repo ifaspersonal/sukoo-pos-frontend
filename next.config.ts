ab09178dee878594e0d3420540c9575f4cc4732e
mantul <mantul@anon.com>
2026-02-12 00:13:45 +0700

    Remove invalid allowedDevOrigins config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3000",
-      "http://192.168.18.117:3000",
-      "http://192.168.18.117",
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
